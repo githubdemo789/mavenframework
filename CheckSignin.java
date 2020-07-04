f8fa1792749fc315e5ed0fa8321cd56b702256b9
priya <krishnapriyakondeti81@gmail.com>
2020-07-04 22:05:25 +0530

    1st commit

---
+++ CheckSignin.java
@@ -0,0 +1,15 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CheckSignin {
+	WebDriver ldriver;
+	
+	 CheckSignin(WebDrive rdriver)
+	 {
+	rdriver=ldriver;
+	
+		 
+	 }
+
+}
